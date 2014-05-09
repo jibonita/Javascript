f2fdbb819b0b77dad9b747512aee9255fdd962e6
jibonita <bamishore@gmail.com>
2014-05-10 00:37:10 +0300

    Op&Exp - task 5-9

--- TELERIK - Javascript +/1.OperatorsExpressions/scripts/my-js.js
+++ TELERIK - Javascript +/1.OperatorsExpressions/scripts/my-js.js
@@ -1,29 +1,28 @@
-function Exec(inputMsg, selector, outputmsg, handler, isPrintOrReturn){
-	//input message for the user
-	if (inputMsg.length == 0) {
-		// default message  
-		inputMsg = 'Please enter an integer number: ';
-	};
-	jsConsole.write(inputMsg);
+function Exec(inputMsg, selector, outputmsg, handler, isPrintOrReturn) {
+    //input message for the user
+    if (inputMsg.length == 0) {
+        // default message  
+        inputMsg = 'Please enter an integer number: ';
+    };
+    jsConsole.write(inputMsg);
 
-	jsConsole.addInputField(selector, function(e){
-		if(e.keyCode === 13){
+    jsConsole.addInputField(selector, function(e) {
+        if (e.keyCode === 13) {
 
-            var value = jsConsole.read('#'+selector);
-			var result = handler(value);
+            var value = jsConsole.read('#' + selector);
+            var result = handler(value);
 
-			// remove the focus of the input field
-			this.blur();
-			this.disabled = true;
+            // remove the focus of the input field
+            this.blur();
+            this.disabled = true;
 
-			//jsConsole.writeLine(selector+":"+(isPrintOrReturn=== false));
-			if(typeof isPrintOrReturn === false){
-				return result;
-			} else {
-				jsConsole.write(outputmsg);
-				jsConsole.write(result);
-       		}
-
-		} 
-	});
+            //jsConsole.writeLine(selector+":"+(isPrintOrReturn=== false));
+            if (typeof isPrintOrReturn === false) {
+                return result;
+            } else {
+                jsConsole.write(outputmsg);
+                jsConsole.write(result);
+            }
+        }
+    });
 }
