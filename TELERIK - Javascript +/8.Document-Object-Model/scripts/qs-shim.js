aba3ff35e568f393b56bfe7b117085ef3e385100
jibonita <bamishore@gmail.com>
2014-06-03 09:36:47 +0300

    DOM: querySelector shim task started

--- TELERIK - Javascript +/8.Document-Object-Model/scripts/qs-shim.js
+++ TELERIK - Javascript +/8.Document-Object-Model/scripts/qs-shim.js
@@ -1,11 +1,38 @@
-if (!document.querySelector) {
-    document.querySelector = function(selectors) {
-        return querySelector(selectors);
-    }
-}
+// if (!document.querySelector) {
+//     document.querySelector = function(selectors) {
+//         return querySelector(selectors);
+//     }
+// }
+
+// http://www.w3schools.com/cssref/css_selectors.asp
 
 var log = console.log;
 
-var querySelector = function(selector) {
+var querySelector = function(elementSelector) {
 
+    //TODO........: remove double spacing in elementSelector
+    // > ignores spaces before and after
+    // : pseudo class does not allow space before and after
+
+    var selectors = elementSelector.split(' '),
+        objects = [],
+        selector;
+
+    for (var i = 0; i < selectors.length; i++) {
+        selector = selectors[i];
+
+        //** check the first symbol if special
+        var firstSymbol = selector[0];
+        switch (firstSymbol) {
+            case '#':
+
+            case '.':
+        }
+    }
+
+    console.log(selectors)
 }
+
+//#._
+var sel = '#red div a:hover';
+querySelector(sel)
