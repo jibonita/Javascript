74a0f3e53ebdfd3a8652b70126b08b20a145d103
jibonita <bamishore@gmail.com>
2014-06-04 10:29:13 +0300

    Canvas HW - dwarf (2)

--- TELERIK - Javascript +/9. HTML5-Canvas/scripts/canvas-drawings.js
+++ TELERIK - Javascript +/9. HTML5-Canvas/scripts/canvas-drawings.js
@@ -1,25 +1,19 @@
 (function() {
+    var ctx;
+
     drawDwarf();
     //  drawBike();
     //  drawHouse();
 
     function drawDwarf() {
         var canvas = document.getElementById('canvas-dwarf');
-        var ctx = canvas.getContext('2d');
+        ctx = canvas.getContext('2d');
 
+        drawHead();
 
-        // ctx.moveTo(280, 175);
-        // draw hat
-        ctx.beginPath();
+        drawHat();
 
-        // hat base
-        drawEllipse(ctx, 130, 25, 150, 175, 0, 2 * Math.PI);
-        // hat cylinder
-        drawEllipse(ctx, 68, 25, 150, 155, 0, 2 * Math.PI);
-
-
-        ctx.closePath();
-        ctx.stroke();
+        //TODO: Eys, nose, mouth
 
     }
     // a - long radius, b - short radius, x,y - center coords
@@ -29,9 +23,52 @@
         var scale = b / a;
         // console.log('scale=' + scale);
         // console.log('x=' + x + ',y = ' + y / scale + ', r=' + a + ', from=' + from + ', to=' + to);
+        ctx.save();
         ctx.scale(1, scale);
         ctx.moveTo(x + a, y / scale);
         ctx.arc(x, y / scale, a, from, to);
-        ctx.scale(1, 1);
+        ctx.restore();
+    }
+
+    function drawHat() {
+        ctx.beginPath();
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = "#1f1913";
+        ctx.fillStyle = "#396693";
+
+        // hat base
+        drawEllipse(ctx, 130, 25, 150, 175, 0, 2 * Math.PI);
+
+        ctx.stroke();
+        ctx.fill();
+        ctx.beginPath();
+
+        // hat cylinder
+        ctx.fillRect(82, 32, 136, 123);
+        drawEllipse(ctx, 68, 25, 150, 155, 0, Math.PI);
+        drawEllipse(ctx, 68, 25, 150, 32, 0, 2 * Math.PI);
+
+        //vertical part of the cylinder
+        ctx.lineWidth = 2;
+        ctx.lineTo(218, 155);
+        ctx.moveTo(82, 155);
+        ctx.lineTo(82, 32);
+
+        ctx.closePath();
+        ctx.stroke();
+        ctx.fill();
+    }
+
+    function drawHead() {
+        ctx.beginPath();
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = "#1e4e5c";
+        ctx.fillStyle = "#92ccda";
+
+        drawEllipse(ctx, 116, 101, 156, 269, 0, 2 * Math.PI);
+
+        ctx.closePath();
+        ctx.stroke();
+        ctx.fill();
     }
 })();
