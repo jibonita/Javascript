54c2ad827eb46793e46a5be8274295cee70bb535
jibonita <bamishore@gmail.com>
2014-07-03 13:29:21 +0300

    Google Logo: added room clock
    1st steps with Kinetic-JS

--- GameS/Fun/Google-Logo-23-june-2014/scripts/room-widgets.js
+++ GameS/Fun/Google-Logo-23-june-2014/scripts/room-widgets.js
@@ -11,32 +11,86 @@
     function drawRoomChart(x, y) {}
 
     function drawClock(x, y) {
+        var clockColor = '#87888c',
+            clockInnerColor = '#cccdce',
+            radius = 29,
+            outlineWidth = 4,
+            centerX = radius,
+            centerY = radius,
+            hourLineShift = 6,
+            hourLineLength = 6,
+            hourLineWidth = 2;
+        var line,
+            group = new Kinetic.Group();
 
-        var arc = new Kinetic.Arc({
-            innerRadius: 40,
-            outerRadius: 80,
-            fill: 'red',
-            stroke: 'black',
-            strokeWidth: 5,
-            angle: 60,
-            rotationDeg: -120
+        group.add(
+            // clock outline
+            new Kinetic.Circle({
+                x: centerX,
+                y: centerY,
+                radius: radius,
+                stroke: clockColor,
+                strokeWidth: outlineWidth,
+                fill: clockInnerColor
+            }),
+            //quarters hour lines
+            line = new Kinetic.Line({
+                points: [hourLineShift, centerY, hourLineShift + hourLineLength, centerY],
+                stroke: clockColor,
+                tension: hourLineWidth
+            }),
+            line.clone({
+                x: 2 * radius - (hourLineShift + hourLineLength) - hourLineLength,
+                y: 0
+            }),
+            line = new Kinetic.Line({
+                points: [centerX, hourLineLength, centerX, hourLineShift + hourLineLength],
+                stroke: clockColor,
+                tension: hourLineWidth
+            }),
+            line.clone({
+                x: 0,
+                y: 2 * radius - (hourLineShift + hourLineLength) - hourLineLength
+            }),
+            // hands
+            line = new Kinetic.Path({
+                data: 'M34,15 38,14 39,17 M38,14 29,29 40,31 M37,29 40,31 37,33',
+                stroke: clockColor,
+                strokeWidth: hourLineWidth
+            })
+        );
+
+        group.move({
+            x: x,
+            y: y
         });
 
-        return arc;
+        layer.add(group);
+        stage.add(layer);
     }
 
     function drawTVScreen(x, y) {}
 
-    function drawWaterMachine(x, y) {}
+    function drawWaterMachine(x, y) {
+        var line,
+            group = new Kinetic.Group();
+
+        // TODO
+
+
+
+
+        layer.add(group);
+        stage.add(layer);
+    }
 
     drawRoomChart(135, 27);
 
-    layer.add(drawClock(135, 27));
-    stage.add(layer);
+    drawClock(618, 72);
 
     drawTVScreen(135, 27);
 
-    drawWaterMachine(135, 27);
+    drawWaterMachine(810, 170);
 
 
 })();
