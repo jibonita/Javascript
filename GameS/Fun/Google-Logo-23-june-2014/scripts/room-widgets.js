c83c8c742d58087f1cb9db289d14e033dd6551ad
jibonita <bamishore@gmail.com>
2014-07-19 00:32:08 +0300

    added drawChair function with KineticJS.
    I want to compare the quality between KineticJS and RaphaelJS

--- GameS/Fun/Google-Logo-23-june-2014/scripts/room-widgets.js
+++ GameS/Fun/Google-Logo-23-june-2014/scripts/room-widgets.js
@@ -1,25 +1,36 @@
-(function() {
+define(['jquery', 'kinetic', 'settings'], function($, Kinetic, Settings) {
+
     var stage = new Kinetic.Stage({
-        container: containerID,
-        width: containerWidth,
-        height: containerHeight
+        container: Settings.container.containerID,
+        width: Settings.container.containerWidth,
+        height: Settings.container.containerHeight
     });
     var layer = new Kinetic.Layer();
-    $('#' + containerID).find('.kineticjs-content').css('position', 'absolute');
-    $('#' + containerID).find('.kineticjs-content').addClass('the-canvas');
+    $('#' + Settings.container.containerID).find('.kineticjs-content').css('position', 'absolute');
+    $('#' + Settings.container.containerID).find('.kineticjs-content').addClass('the-canvas');
 
     function drawRoomChart(x, y) {}
 
     function drawClock(x, y) {
-        var clockColor = '#87888c',
-            clockInnerColor = '#cccdce',
-            radius = 29,
-            outlineWidth = 4,
-            centerX = radius,
-            centerY = radius,
-            hourLineShift = 6,
-            hourLineLength = 6,
-            hourLineWidth = 2;
+        // var clockColor = '#87888c',
+        //     clockInnerColor = '#cccdce',
+        //     radius = 29,
+        //     outlineWidth = 4,
+        //     centerX = radius,
+        //     centerY = radius,
+        //     hourLineShift = 6,
+        //     hourLineLength = 6,
+        //     hourLineWidth = 2;
+        var settings = Settings.roomClock;
+        var clockColor = settings.clockColor,
+            clockInnerColor = settings.clockInnerColor,
+            radius = settings.radius,
+            outlineWidth = settings.outlineWidth,
+            centerX = settings.centerX,
+            centerY = settings.centerY,
+            hourLineShift = settings.hourLineShift,
+            hourLineLength = settings.hourLineLength,
+            hourLineWidth = settings.hourLineWidth;
         var line,
             group = new Kinetic.Group();
 
@@ -84,13 +95,85 @@
         stage.add(layer);
     }
 
-    drawRoomChart(135, 27);
+    function drawChairs(x, y) {
+        //paper.clear();
 
-    drawClock(618, 72);
+        var foot,
+            //st = paper.set(),
+            leftChairXShift = 85,
+            leftChairYShift = 235;
 
-    drawTVScreen(135, 27);
+        var group = new Kinetic.Group(),
+            feet = new Kinetic.Group();
 
-    drawWaterMachine(810, 170);
+        feet.add(
+            foot = new Kinetic.Rect({
+                x: 2,
+                y: 51,
+                width: 3,
+                height: 13
+            }),
+            foot.clone({
+                x: foot.attrs.x + 45,
+                y: foot.attrs.y
+            }),
+            new Kinetic.Rect({
+                x: 24,
+                y: 51,
+                width: 3,
+                height: 5
+            }),
+            new Kinetic.Rect({
+                x: 63,
+                y: 42,
+                width: 3,
+                height: 14
+            })
+        );
+        feet.setAttrs({
+            fill: '#696666'
+        });
+
+        // Left chair shift from room: x:85, y: 235
+        group.add(
+
+            new Kinetic.Path({
+                data: 'M0,0 0,51 5,51 5,0 z',
+                fill: '#393c3c'
+            }),
+            new Kinetic.Path({
+                data: 'M5,44 5,51 52,51 52,44 z',
+                fill: '#353332'
+            }),
+            feet,
+            new Kinetic.Path({
+                data: 'M5,44 52,44 52,51 76,39 76,34 28,34 z',
+                fill: '#bab8b3'
+            }),
+            new Kinetic.Path({
+                data: 'M5,0 5,44 28,34 28,0 z',
+                fill: '#5c5e60',
+            })
+        );
 
 
-})();
+        // st.transform('t' + (x + leftChairXShift) + ',' + (y + leftChairYShift));
+        group.move({
+            x: x,
+            y: y
+        });
+
+        layer.add(group);
+        stage.add(layer);
+    }
+
+
+    return {
+        drawRoomChart: drawRoomChart,
+        drawClock: drawClock,
+        drawTVScreen: drawTVScreen,
+        drawWaterMachine: drawWaterMachine,
+        drawChairs: drawChairs
+    };
+
+});
