55ff7a56187315e3d3129bd24e181862016f748a
jibonita <bamishore@gmail.com>
2014-07-22 00:27:06 +0300

    data files for the underscore-js HW

---
+++ TELERIK - Javascript +/Javascript Apps +/24. Underscore JS/scripts/data/animals.js
@@ -0,0 +1,69 @@
+// I used this site to generate data: http://www.json-generator.com/
+// Code generator:
+// var animals = ['ant', 'bird', 'cat', 'chicken', 'cow', 'dog', 'elephant', 'fish', 'fox', 'horse', 'kangaroo', 'lion', 'monkey', 'penguin', 'pig', 'rabbit', 'sheep', 'tiger', 'whale', 'wolf'];
+
+// [
+//   '{{repeat(50)}}',
+//   {
+//     id: '{{index()}}',
+//     legs: '{{random(2, 4, 6, 8, 100)}}',
+//     specie: function (tags) {
+//       return animals[tags.integer(0, animals.length - 1)];
+//     }
+//   }
+// ]
+//** PLEASE NOTE THAT ANIMAL SPECIE AND NUMBER OF LEGS ARE NOT CONSISTENT IN THIS JSON ABJECT!!!!!!!
+
+var animals = [{
+    "id": 0,
+    "legs": 100,
+    "specie": "cow"
+}, {
+    "id": 1,
+    "legs": 2,
+    "specie": "elephant"
+}, {
+    "id": 2,
+    "legs": 4,
+    "specie": "kangaroo"
+}, {
+    "id": 3,
+    "legs": 4,
+    "specie": "bird"
+}, {
+    "id": 4,
+    "legs": 8,
+    "specie": "cow"
+}, {
+    "id": 5,
+    "legs": 4,
+    "specie": "elephant"
+}, {
+    "id": 6,
+    "legs": 2,
+    "specie": "fox"
+}, {
+    "id": 7,
+    "legs": 4,
+    "specie": "sheep"
+}, {
+    "id": 8,
+    "legs": 6,
+    "specie": "wolf"
+}, {
+    "id": 9,
+    "legs": 4,
+    "specie": "fish"
+}, {
+    "id": 10,
+    "legs": 100,
+    "specie": "rabbit"
+}, {
+    "id": 11,
+    "legs": 6,
+    "specie": "tiger"
+}, {
+    "id": 12,
+    "legs": 2,
+    "specie": "horse"
+}];
---
+++ TELERIK - Javascript +/Javascript Apps +/24. Underscore JS/scripts/data/students.js
@@ -0,0 +1,437 @@
+// I used this site to generate data: http://www.json-generator.com/
+// Code generator:
+// [
+//   '{{repeat(30)}}',
+//   {
+//     firstname: '{{firstName()}}',
+//     lastname: '{{surname()}}',
+//     age: '{{integer(5, 40)}}',
+//     gender: '{{gender()}}',
+//     marks: [
+//       '{{repeat(7)}}',
+//       '{{integer(2, 10)}}'
+//     ]
+//   }
+// ]
+
+var students = [{
+    "firstname": "Nielsen",
+    "lastname": "Morrison",
+    "age": 12,
+    "gender": "male",
+    "marks": [
+        6,
+        5,
+        7,
+        5,
+        2,
+        3,
+        4
+    ]
+}, {
+    "firstname": "Dina",
+    "lastname": "Schwartz",
+    "age": 8,
+    "gender": "female",
+    "marks": [
+        6,
+        5,
+        10,
+        2,
+        7,
+        2,
+        6
+    ]
+}, {
+    "firstname": "Nora",
+    "lastname": "Torres",
+    "age": 11,
+    "gender": "female",
+    "marks": [
+        4,
+        9,
+        8,
+        7,
+        2,
+        5,
+        8
+    ]
+}, {
+    "firstname": "Hayden",
+    "lastname": "Carson",
+    "age": 36,
+    "gender": "male",
+    "marks": [
+        5,
+        6,
+        7,
+        3,
+        7,
+        6,
+        10
+    ]
+}, {
+    "firstname": "Best",
+    "lastname": "Garrett",
+    "age": 32,
+    "gender": "male",
+    "marks": [
+        10,
+        5,
+        5,
+        3,
+        10,
+        3,
+        4
+    ]
+}, {
+    "firstname": "Virginia",
+    "lastname": "Clarke",
+    "age": 37,
+    "gender": "female",
+    "marks": [
+        7,
+        10,
+        5,
+        5,
+        7,
+        6,
+        10
+    ]
+}, {
+    "firstname": "Pearson",
+    "lastname": "Hyde",
+    "age": 18,
+    "gender": "male",
+    "marks": [
+        10,
+        9,
+        9,
+        3,
+        6,
+        4,
+        7
+    ]
+}, {
+    "firstname": "Janet",
+    "lastname": "Cabrera",
+    "age": 20,
+    "gender": "female",
+    "marks": [
+        8,
+        10,
+        5,
+        3,
+        6,
+        5,
+        3
+    ]
+}, {
+    "firstname": "Hernandez",
+    "lastname": "Ware",
+    "age": 33,
+    "gender": "male",
+    "marks": [
+        10,
+        10,
+        6,
+        2,
+        6,
+        4,
+        6
+    ]
+}, {
+    "firstname": "Rowland",
+    "lastname": "Good",
+    "age": 8,
+    "gender": "male",
+    "marks": [
+        9,
+        3,
+        9,
+        2,
+        3,
+        8,
+        5
+    ]
+}, {
+    "firstname": "Robin",
+    "lastname": "Keith",
+    "age": 5,
+    "gender": "female",
+    "marks": [
+        3,
+        10,
+        5,
+        3,
+        3,
+        4,
+        7
+    ]
+}, {
+    "firstname": "Burch",
+    "lastname": "Harris",
+    "age": 33,
+    "gender": "male",
+    "marks": [
+        5,
+        2,
+        10,
+        5,
+        10,
+        9,
+        3
+    ]
+}, {
+    "firstname": "Deirdre",
+    "lastname": "Barron",
+    "age": 25,
+    "gender": "female",
+    "marks": [
+        5,
+        2,
+        7,
+        9,
+        8,
+        8,
+        8
+    ]
+}, {
+    "firstname": "Sims",
+    "lastname": "Beard",
+    "age": 36,
+    "gender": "male",
+    "marks": [
+        5,
+        10,
+        4,
+        9,
+        2,
+        8,
+        3
+    ]
+}, {
+    "firstname": "Josie",
+    "lastname": "Randall",
+    "age": 20,
+    "gender": "female",
+    "marks": [
+        10,
+        10,
+        6,
+        4,
+        8,
+        4,
+        2
+    ]
+}, {
+    "firstname": "Hickman",
+    "lastname": "Howard",
+    "age": 38,
+    "gender": "male",
+    "marks": [
+        2,
+        5,
+        8,
+        2,
+        8,
+        8,
+        4
+    ]
+}, {
+    "firstname": "Grimes",
+    "lastname": "Walter",
+    "age": 19,
+    "gender": "male",
+    "marks": [
+        10,
+        3,
+        8,
+        8,
+        9,
+        2,
+        7
+    ]
+}, {
+    "firstname": "Leah",
+    "lastname": "Mcdonald",
+    "age": 12,
+    "gender": "female",
+    "marks": [
+        3,
+        7,
+        3,
+        4,
+        6,
+        5,
+        7
+    ]
+}, {
+    "firstname": "Madeleine",
+    "lastname": "Valencia",
+    "age": 21,
+    "gender": "female",
+    "marks": [
+        3,
+        4,
+        2,
+        2,
+        6,
+        8,
+        4
+    ]
+}, {
+    "firstname": "Elise",
+    "lastname": "Ray",
+    "age": 6,
+    "gender": "female",
+    "marks": [
+        3,
+        7,
+        5,
+        3,
+        6,
+        2,
+        7
+    ]
+}, {
+    "firstname": "Simon",
+    "lastname": "Gates",
+    "age": 29,
+    "gender": "male",
+    "marks": [
+        6,
+        4,
+        7,
+        9,
+        9,
+        10,
+        3
+    ]
+}, {
+    "firstname": "Estela",
+    "lastname": "Quinn",
+    "age": 29,
+    "gender": "female",
+    "marks": [
+        4,
+        8,
+        2,
+        6,
+        5,
+        7,
+        4
+    ]
+}, {
+    "firstname": "Mejia",
+    "lastname": "Morton",
+    "age": 19,
+    "gender": "male",
+    "marks": [
+        2,
+        9,
+        8,
+        6,
+        5,
+        10,
+        5
+    ]
+}, {
+    "firstname": "Church",
+    "lastname": "Caldwell",
+    "age": 21,
+    "gender": "male",
+    "marks": [
+        9,
+        3,
+        8,
+        5,
+        6,
+        8,
+        2
+    ]
+}, {
+    "firstname": "Murray",
+    "lastname": "Weeks",
+    "age": 19,
+    "gender": "male",
+    "marks": [
+        4,
+        9,
+        6,
+        7,
+        5,
+        9,
+        9
+    ]
+}, {
+    "firstname": "Hart",
+    "lastname": "Kane",
+    "age": 11,
+    "gender": "male",
+    "marks": [
+        10,
+        2,
+        3,
+        6,
+        3,
+        3,
+        10
+    ]
+}, {
+    "firstname": "Tia",
+    "lastname": "Forbes",
+    "age": 21,
+    "gender": "female",
+    "marks": [
+        3,
+        6,
+        2,
+        3,
+        5,
+        5,
+        6
+    ]
+}, {
+    "firstname": "Day",
+    "lastname": "Humphrey",
+    "age": 40,
+    "gender": "male",
+    "marks": [
+        10,
+        6,
+        5,
+        4,
+        9,
+        6,
+        4
+    ]
+}, {
+    "firstname": "Hattie",
+    "lastname": "Ellison",
+    "age": 5,
+    "gender": "female",
+    "marks": [
+        2,
+        7,
+        5,
+        4,
+        4,
+        9,
+        2
+    ]
+}, {
+    "firstname": "Mcpherson",
+    "lastname": "Foley",
+    "age": 10,
+    "gender": "male",
+    "marks": [
+        7,
+        2,
+        10,
+        3,
+        10,
+        7,
+        6
+    ]
+}];
