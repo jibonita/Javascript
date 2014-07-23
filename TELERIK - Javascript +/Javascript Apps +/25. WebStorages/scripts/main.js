2d118d7b64ef9a0e2d2858788153f9cc28f39bb7
jibonita <bamishore@gmail.com>
2014-07-23 20:23:19 +0300

    HW WebStorages - initial files

---
+++ TELERIK - Javascript +/Javascript Apps +/25. WebStorages/scripts/main.js
@@ -0,0 +1,15 @@
+(function() {
+
+    require.config({
+        paths: {
+            'jquery': 'libs/jquery-2.1.1.min',
+            'handlebars': 'libs/handlebars-v1.3.0',
+            'storage': 'web-storage-objects'
+        }
+    });
+
+    require([], function() {
+
+
+    });
+})();
